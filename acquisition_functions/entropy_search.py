gp_model: fixed a bug in variance prediction entropy_search: improved sampling, changed sign of return value entropy_search_big_data: adapted to changes in entropy_search

acquisition_functions/entropy_search.py
@@ -34,12 +34,15 @@ class EntropySearch(object):
         '''
         Default constructor.
         '''
-        self._omega = np.random.normal(0, 1, NUMBER_OF_CAND_SAMPLES)
+        self._Omega = np.random.normal(0, 1, (NUMBER_OF_CAND_SAMPLES,
+                                              NUMBER_OF_PMIN_SAMPLES,
+                                              NUMBER_OF_REPRESENTER_POINTS+1))
         self._gp = gp
         self._cost_gp = cost_gp
         starting_point = comp[np.argmin(vals)]
         self._ei = ExpectedImprovement(comp, vals, gp, cost_gp)
-        self._representers = self.sample_representer_points(starting_point, 
+        self._representers = np.empty([NUMBER_OF_REPRESENTER_POINTS+1,starting_point.shape[0]])
+        self._representers[1:] = self.sample_representer_points(starting_point,
                                                                            self._log_proposal_measure, 
                                                                            NUMBER_OF_REPRESENTER_POINTS)
         
@@ -77,17 +80,17 @@ class EntropySearch(object):
             raise NotImplementedError("computing gradients not supported by this acquisition function")
         gain = 0
         log_proposal_vals = np.zeros(NUMBER_OF_REPRESENTER_POINTS)
-        for o in self._omega:
+        for o in self._Omega:
             pmin = self._compute_pmin_bins(self._gp, candidate, o)
             entropy_pmin = -np.dot(pmin, np.log(pmin+1e-10))
             #TODO:is it necessary to recompute the proposal measure values of the representer points using the GP copy?
             for i in range(0, NUMBER_OF_REPRESENTER_POINTS):
                 log_proposal_vals[i] = self._log_proposal_measure(self._representers[i])
             log_proposal = np.dot(log_proposal_vals, pmin)
-            
-            kl_divergence = entropy_pmin - log_proposal 
+            kl_divergence = entropy_pmin - log_proposal
             gain = gain + kl_divergence
-        return gain
+        #in the paper the acquisition function is minimized but we maximize
+        return -gain
     
     def _compute_pmin_bins(self, gp, candidate, omega):
         '''
@@ -100,19 +103,16 @@ class EntropySearch(object):
         Returns:
             a numpy array with a probability for each representer point
         '''
-        y = gp.sample(candidate,omega)
-        gp_copy = gp.copy()
-        gp_copy.update(candidate, y)
         pmin = np.zeros(NUMBER_OF_REPRESENTER_POINTS)
-        for s in xrange(0,NUMBER_OF_PMIN_SAMPLES):
-            omega2 = npr.normal(0, 1, NUMBER_OF_REPRESENTER_POINTS) #1-dimensional samples
-            vals = gp.drawJointSample(self._representers, omega2)
+        self._representers[0] = candidate
+        for o in omega:
+            vals = gp.drawJointSample(self._representers, o)
             mins = np.where(vals == vals.min())
             number_of_mins = len(mins)
             #mins is a tuple of single valued array
             for m in mins:
                 #have to extract the value of the array
-                pmin[m[0]] += 1/(number_of_mins)
+                pmin[m[0]-1] += 1/(number_of_mins)
         pmin = pmin / NUMBER_OF_PMIN_SAMPLES
         return pmin
     

gp_model.py
@@ -117,6 +117,7 @@ def grad_Linear(ls, x1, x2=None):
     dk = -dk
     return dk
 
+#TODO: kernel has sharp edge close to zero. Is there something we can do about it?
 def LogLinear(ls, x1, x2=None, grad=False):
     '''
     Linear kernel that puts the inputs on a log scale.
@@ -436,9 +437,15 @@ class GPModel(object):
             return func_m
         
         beta = spla.solve_triangular(self._L, kXstar, lower=True)
-        #TODO: change back
+
+        #old spearmint line - their kernels have k(X,X)=1
         #func_v = self._amp2 * (1 + 1e-6) - np.sum(beta ** 2, axis=0)
-        func_v = self._compute_covariance(Xstar) - np.dot(beta.T, beta)
+
+        #prior variance is basically diag(k(X,X))
+        prior_variance = np.empty(Xstar.shape[0])
+        for i in range(0, Xstar.shape[0]):
+            prior_variance[i] = self._compute_covariance(np.array([Xstar[i]]))[0]
+        func_v = prior_variance - np.sum(beta ** 2, axis=0) #np.dot(beta.T, beta)
         return (func_m, func_v)
 
     def predict_vector(self, input_point):